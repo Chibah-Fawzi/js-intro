5577cba2870beadfdf1284fd66d79e11d0ae461a
fawzi <fawzichibah@gmail.com>
2023-10-21 12:49:46 +0100

    js introduction

---
+++ script.js
@@ -0,0 +1,63 @@
+// // console.log("hello world");
+
+// // alert("je suis une alerte");
+
+// // confirm("je suis une confirmation");
+
+// var myName = "Moh";
+// var age = 40;
+
+// console.log(myName + age);
+
+// var x = 200;
+// var y = 1;
+// var z = 40;
+
+// var result = x > y || y > z; // true il faut qu'un seul cas sois juste
+// var result2 = x > y && y > z; // false il faut que tout les cas soient juste
+
+// // Stocké dans la BDD
+// var dbEmail = "test1@gmail.com";
+// var dbPwd = "test1";
+
+// // Ecrit par le user
+// var userEmail = "test1@gmail.com";
+// var userPwd = "test";
+
+// // Conditions
+// if (userEmail === dbEmail && userPwd === dbPwd) {
+//   alert("Vous êtes connecté");
+// } else if (age == 40) {
+//   alert("Cette personne a 40 ans");
+// } else {
+//   alert("Identifients invalides");
+// }
+
+function Bonjour(name) {
+  console.log("Bonjour à vous " + name);
+}
+
+// Function lié avec le button dans l'HTML
+function Greet(name) {
+  alert("Bonjour à vous " + name);
+}
+
+// LOOPS
+
+var chiffreActuel = 10;
+
+// while (chiffreActuel < 10) {
+//   console.log("Le chiffre est de : " + chiffreActuel);
+//   chiffreActuel++;
+// }
+
+// do {
+//   console.log("Je suis la loop do while" + chiffreActuel);
+//   chiffreActuel--;
+// } while (chiffreActuel > 3);
+
+for (var index = 0; index < 10; index++) {
+  console.log("JE SUIS DANS LA LOOP HELPPPPP Indéx n° : " + index);
+}
+
+console.log("JESUIS SORTIEEEEEE ", index);
